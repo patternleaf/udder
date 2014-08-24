41c197eaa7e2c3fbac47186b0632e95786eef843
Mike Bissell <src@coil-lighting.com>
2014-08-24 13:15:00 -0700

    Quick-start utilities to help Java novices get started (temporary).

---
+++ obsolete/install_jars.sh
@@ -0,0 +1,2 @@
+# There must be a better way to do this.
+mvn install:install-file -Dfile=simple-5.1.5.jar  -DgroupId=org.simpleframework -DartifactId=simpleframework -Dversion=5.1.5 -Dpackaging=jar
---
+++ obsolete/mkproject.sh
@@ -0,0 +1,1 @@
+mvn archetype:generate -DgroupId=com.coillighting.udder -DartifactId=udder -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
