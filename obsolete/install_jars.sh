6693a5b54aa338625201ef4d7f1d023030e1d0db
Mike Bissell <src@coil-lighting.com>
2014-08-26 17:50:33 -0700

    MNT: remove some obsolete shell scripts

--- obsolete/install_jars.sh
+++
@@ -1,2 +0,0 @@
-# There must be a better way to do this.
-mvn install:install-file -Dfile=simple-5.1.5.jar  -DgroupId=org.simpleframework -DartifactId=simpleframework -Dversion=5.1.5 -Dpackaging=jar
--- obsolete/mkproject.sh
+++
@@ -1,1 +0,0 @@
-mvn archetype:generate -DgroupId=com.coillighting.udder -DartifactId=udder -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
