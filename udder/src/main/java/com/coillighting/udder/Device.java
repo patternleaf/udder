08f6d598a711ec934a3ac8c6d93f4c64df326bbd
Mike Bissell <src@coil-lighting.com>
2014-09-13 12:45:04 -0700

    CFG: a provisional JSON patch sheet, as exported by Eric 8/19/2014. REF: use floating point values for 3D positions. ENH: add a simple grouping field to Device.

--- udder/src/main/java/com/coillighting/udder/Device.java
+++ udder/src/main/java/com/coillighting/udder/Device.java
@@ -12,13 +12,20 @@ public class Device extends Object {
 	 */
 	private long addr = 0;
 
-	private long x = 0;
-	private long y = 0;
-	private long z = 0;
+	/** A dirt simple grouping mechanism. Each Device belongs to exactly one
+	 *  group (for now). For the Dairy installation, this will indicate gate
+	 *  0 or gate 1, in case an Animator cares which group the Device is in.
+	 */
+	private long group=0;
 
-	public void Device(long addr, long x, long y, long z) {
+	/** Position in model space. */
+	private double x = 0;
+	private double y = 0;
+	private double z = 0;
+
+	public void Device(long addr, long group, double x, double y, double z) {
 		this.addr = addr;
-
+		this.group = group;
 		this.x = x;
 		this.y = y;
 		this.z = z;
