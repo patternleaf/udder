6c8faaa12eec5d8c2bde05183f110d27bfb15efb
Mike Bissell <src@coil-lighting.com>
2014-10-12 11:51:18 -0700

    REF: shorten envelope name

--- udder/src/main/java/com/coillighting/udder/signal/SharkfinFloatSignal.java
+++ udder/src/main/java/com/coillighting/udder/signal/SharkfinFloatSignal.java
@@ -5,9 +5,9 @@ import com.coillighting.udder.Util;
 /** A sinusoidally interpolated signal that continuously oscillates from start
  *  to end, then discontinuously jumps back to the start value.
  */
- public class SinusoidalSawFloatSignal extends FloatSignalBase {
+ public class SharkfinFloatSignal extends FloatSignalBase {
 
-    public SinusoidalSawFloatSignal(float start, float end, long period) {
+    public SharkfinFloatSignal(float start, float end, long period) {
         super(start, end, period);
     }
 
