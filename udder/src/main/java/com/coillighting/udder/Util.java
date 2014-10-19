584964eddc1c69e786230c763754b35df1c5be6d
Mike Bissell <src@coil-lighting.com>
2014-10-19 14:19:14 -0700

    REF: allow users to skip linear interpolation if they know their parameter range is prenormalized to [0.0...0.1].

--- udder/src/main/java/com/coillighting/udder/Util.java
+++ udder/src/main/java/com/coillighting/udder/Util.java
@@ -42,13 +42,26 @@ public class Util {
      *  (0, y0) and (1, y1). If you supply an x that is out of range, the
      *  result will likewise be out of range.
      */
-    public static double crossfadeLinear(double x, double y0, double y1) {
+    public static final double crossfadeLinear(double x, double y0, double y1) {
         return y0 + x * (y1 - y0);
     }
 
     /** A 32-bit version of crossfadeLinear. */
-    public static float crossfadeLinear(float x, float y0, float y1) {
+    public static final float crossfadeLinear(float x, float y0, float y1) {
         return y0 + x * (y1 - y0);
+    }
+
+    /** A version of crossfadeExponential optimized for normalized scenarios
+     *  where y0=0, y1=1, and x is between 0 and 1 inclusive. Reduces to
+     *  x^exponent.
+     */
+    public static final double reshapeExponential(double x, double exponent) {
+        return Math.pow(x, exponent);
+    }
+
+    /** A mostly 32-bit version of reshapeExponential. */
+    public static final float reshapeExponential(float x, float exponent) {
+        return (float) Math.pow((double) x, (double) exponent);
     }
 
     /** For a fast attack followed by a slower follow-through, try an exponent
@@ -56,41 +69,53 @@ public class Util {
      *  try an exponent of 2-5. This is not really the shapliest easing curve
      *  curve out there, but it's good to have in the arsenal.
      */
-    public static double crossfadeExponential(double x, double exponent, double y0, double y1) {
-        double balance = Math.pow(x, exponent);
+    public static final double crossfadeExponential(double x, double exponent, double y0, double y1) {
+        double balance = reshapeExponential(x, exponent);
         return crossfadeLinear(balance, y0, y1);
     }
 
     /** A mostly 32-bit version of crossfadeExponential. */
-    public static float crossfadeExponential(float x, float exponent, float y0, float y1) {
-        float balance = (float) Math.pow((double) x, (double) exponent);
+    public static final float crossfadeExponential(float x, float exponent, float y0, float y1) {
+        float balance = reshapeExponential(x, exponent);
         return crossfadeLinear(balance, y0, y1);
     }
 
-    /** Use a sinewave to approximate a sigmoidal cross-fade.
-     *  Submit this expression to wolframalpha.com to see the resulting curve:
-     *      0.5*(1 + (sin((pi*(x-0.5)))))
-     *  Direct link:
-     *      http://www.wolframalpha.com/input/?i=0.5*%281+%2B+%28sin%28%28pi*%28x-0.5%29%29%29%29%29+from+-0.25+to+1.5
+    /** Like crossfadeSinusoidal, but optimized for normalized input scenarios
+     *  where y0=0, y1=1, and is is between 0 and 1 inclusive.
      */
-    public static double crossfadeSinusoidal(double x, double y0, double y1) {
+    public static final double reshapeSinusoidal(double x) {
         // Map an x in range 0..1 to an xx in range -pi..0.
         double xx = (x - 0.5) * Math.PI;
         double yy = Math.sin(xx);
 
-        // Map a yy in range -1..1 to a balance in range y0..y1.
-        double balance = 0.5 * (yy + 1.0);
-        return crossfadeLinear(balance, y0, y1);
+        // Map a yy in range -1..1 to a balance in range 0..1.
+        return 0.5 * (yy + 1.0);
     }
 
-    /** A mostly 32-bit version of crossfadeExponential. */
-    public static float crossfadeSinusoidal(float x, float y0, float y1) {
+    /** A mostly 32-bit version of reshapeSinusoidal. */
+    public static final float reshapeSinusoidal(float x) {
         // Map an x in range 0..1 to an xx in range -pi..0.
         double xx = ((double)x - 0.5) * Math.PI;
         float yy = (float) Math.sin(xx);
 
         // Map a yy in range -1..1 to a balance in range y0..y1.
-        float balance = 0.5f * (yy + 1.0f);
+        return 0.5f * (yy + 1.0f);
+    }
+
+    /** Use a sinewave to approximate a sigmoidal cross-fade curve.
+     *  Submit this expression to wolframalpha.com to see the resulting curve:
+     *      0.5*(1 + (sin((pi*(x-0.5)))))
+     *  Direct link:
+     *      http://www.wolframalpha.com/input/?i=0.5*%281+%2B+%28sin%28%28pi*%28x-0.5%29%29%29%29%29+from+-0.25+to+1.5
+     */
+    public static final double crossfadeSinusoidal(double x, double y0, double y1) {
+        double balance = reshapeSinusoidal(x);
+        return crossfadeLinear(balance, y0, y1);
+    }
+
+    /** A mostly 32-bit version of crossfadeExponential. */
+    public static final float crossfadeSinusoidal(float x, float y0, float y1) {
+        float balance = reshapeSinusoidal(x);
         return crossfadeLinear(balance, y0, y1);
     }
 
