a141929daefa642101ae455137cd6549881e3073
Mike Bissell <src@coil-lighting.com>
2015-04-05 12:03:49 -0700

    LD: boost amber notes, reduce redness of initial fireball effect to compensate for the color profile differences between my monitor and the Dairy's rig, whose red LEDs are way too hot. DOC: delete obsolete notes.

--- udder/src/main/java/com/coillighting/udder/scene/DairyScene.java
+++ udder/src/main/java/com/coillighting/udder/scene/DairyScene.java
@@ -32,7 +32,7 @@ import com.coillighting.udder.model.Pixel;
  */
 public abstract class DairyScene {
 
-    /** Instantiate a new scene in the form of a Mixer. */
+    /** Instantiate a new, site-specific scene in the form of a Mixer. */
     public static Mixer create(Device[] devices) {
         BlendOp max = new MaxBlendOp();
 
@@ -62,10 +62,29 @@ public abstract class DairyScene {
         String adir = "images" + separator + "dairy_collection_A_720p" + separator;
         String loopdir = "images" + separator + "dairy_collection_B_scrolling_loops" + separator;
 
+        // Warning: adding a few more layers than this may overload some
+        // (currently unknown) stage of the pipeline when run on the
+        // Dairy's little Beaglebone server. This causes quasiperiodic
+        // sticky-looking frame drops of usually about 1/3 second every
+        // approx. 4 sec. These are especially visible when the shuffled
+        // texture layers are visible, but you can also see in the Woven
+        // effect. The Fadecandy's interpolation tries to paper over the
+        // missing frames, but it hurts the brain to look at. I speculate
+        // that this has something to do with thread scheduling, because
+        // I've never been able to reproduce this bug on my multicore
+        // MBP. Especially weird is that dropping the frame rate by 50%
+        // or even 75% doesn't seem to change the sticky playback. That is
+        // big clue #1. Two more  clues: 2) the CPU is not quite maxxed out
+        // (according to htop) while this bug is visible, and running htop
+        // or not (which occupies 9% CPU itself) doesn't obviously change
+        // the stickiness. 3) The logs never report dropped frames, leading
+        // me to speculate that Udder never rendered those frames to begin
+        // with.
+
         RollEffect r;
         ArrayList<EffectSlot> fx = new ArrayList<EffectSlot>();
 
-        r = new RollEffect(loopdir + "flame_scroller_high_key_high_contrast.png");
+        r = new RollEffect(loopdir + "flame_scroller_amber_high_key_high_contrast.png");
         r.setYPeriodMillis(1450);
         fx.add(new EffectSlot(r, 1.0, 0.45));
 
@@ -236,127 +255,6 @@ public abstract class DairyScene {
     }
 
 }
-
-// Old scenes, ideas, and notes
-
-// This playlist looked nice, and it played back smoothly, but there
-// weren't enough open patterns between the dark, detailed ones to
-// sufficiently illuminate the whole structure.
-        /*
-        String [] sequencedTextures_A = {
-                adir + "blue_skull_necklace.png", // ** blue lightning, good reviews from BV
-                adir + "green_gilled_lace.png",
-                adir + "yellow_antennae.png",
-                adir + "cyan_chains.png",
-                adir + "magenta_loops.png",
-                adir + "yellow_lavender_calligraphic.png",
-                adir + "purple_chains.png",
-                adir + "amber_mustachioed_cthulus.png", // too sparse, might modify
-                adir + "orange_mustachioed_cthulus.png",  // too sparse, might modify
-                adir + "purple_blue_chains.png", // see also its twin
-                adir + "red_triclops_minimal.png", // too minimal
-                adir + "red_triclops_embellished.png",
-                adir + "light_blue_calligraphemes.png", // probably twinkly, nonreflective off copper
-                adir + "lavender_propellers.png", // overlay
-                adir + "skyblue_loops.png", // for an overlay?
-                adir + "mauve_taupe_worms.png", // scheduled
-                adir + "redblue_triclops.png", // scheduled, boost brightness
-        };
-        */
-
-        /*
-ideas
------
-flame_scroller_high_key_high_contrast > (orange, mix early, keep late) because people are scared of the dark
-blue_skull_necklace  > (mix late) mostly alone
-light_cyan_trigrams  | (mix late) [should blend nicely with blue above]
-light_amber_trigrams | (mix early) mix all three. verified good.
-coppertone_trigrams  | (mix early, keep late)                 <<< tree shadows, try to solo on outro
-
-flame_scroller_high_key_low_contrast > bright amber, return of the flame
-
-redblue_triclops - boost blue brightness? or use med contrast version? nice palette, nice interaction but too dim
-
-rose_tint_trigrams purple sparks, brings out copper, full enough but still dim
-
-cartoon_flame_scroller_horizontal_* (mix earlyish, maybe flip v?)
-
-rainbow stupidity 1 (mix latish) - maybe tone down primary reds in these a little more? check.
-
-?? amber_mustachioed_cthulus.png (desparsify, hue-diversify background?)
-
-cartoon_rivulet_scroller_horizontal_* (maybe flip h or v?)
-
-rainbow stupidity 2 (mix latish)
-
-purple_chains.png - but fill in black holes "subtle but tasteful w/ deep violet" kind of dark
-    see also purple_blue_chains, which got cut previously
-    (maybe use warmed skyblue_loops as subtle amber overlay?)
-mauve_taupe_worms - not bad on metal, pinkish
-
-pastel rainbow stupidity here?
-- or -
-flame_scroller_low_key_high_contrast > more amber, return of the flame
-- or -
-something not made yet? colorful, structured to fit with the weave -- stacked hexes + blur? simple v-scroll?
-        */
-
-
-// The following playlist sufficiently illuminates the structure,
-// but the nice, detailed motion gets washed out because there
-// are so many open patterns.
-//
-// Warning: adding a few more layers than this overloads some
-// (currently unknown) stage of the pipeline when run on the
-// Dairy's little Beaglebone server. This causes quasiperiodic
-// sticky-looking frame drops of usually about 1/3 second every
-// approx. 4 sec. These are especially visible when the shuffled
-// texture layers are visible, but you can also see in the Woven
-// effect. The Fadecandy's interpolation tries to paper over the
-// missing frames, but it hurts the brain to look at. I speculate
-// that this has something to do with thread scheduling, because
-// I've never been able to reproduce this bug on my multicore
-// MBP. Especially weird is that dropping the frame rate by 50%
-// or even 75% doesn't seem to change the sticky playback. That is
-// big clue #1. Two more  clues: 2) the CPU is not quite maxxed out
-// (according to htop) while this bug is visible, and running htop
-// or not (which occupies 9% CPU itself) doesn't obviously change
-// the stickiness. 3) The logs never report dropped frames, leading
-// me to speculate that Udder never rendered those frames to begin
-// with.
-        /*
-        String [] sequencedTextures_AandB = {
-                bdir + "flames.jpg", // 2
-                adir + "amber_mustachioed_cthulus.png", // 3 nice effect but rather sparse
-                bdir + "flower_pedals.jpg", // 4 offensive
-                adir + "rose_tint_trigrams.png", // 5 ** purple sparks, looks good, brings out copper, full enough, dim but distinct look
-                bdir + "luminous_purple_flower.jpg", // 6
-                // **? 5 + 7: purple twinkles + amber/purple twinkles? ok, lots of motion, full, twinkly, not actually in the playlist though
-                adir + "coppertone_trigrams.png", // 7 ** nice copper mode. tree shadows in wind.
-                bdir + "orange_clouds.jpg", // 8
-                adir + "light_amber_trigrams.png", // 9 ** light amber shimmers, sort of a flattering brown on the metal strips, nice contrast between various strips.
-                bdir + "pink_yellow_orange_flowers.jpg", // 10 clown barf, wtf?
-                // ** 9 + 11 + 7 looks good
-                adir + "light_cyan_trigrams.png", // 11 light blue "lightning" -- maybe not as electric as the pure blue one they cut. nice when it jumps fast.
-                bdir + "orange_green_geometry.jpg", // 12
-                adir + "orange_mustachioed_cthulus.png", // 13 too many black gaps, maybe too primary? notes are conflicted.
-                bdir + "purple_on_green_flower.jpg", // 14
-                adir + "redblue_triclops.png", // 15 red spots with violet undertones, interacts nicely with the rig but rather sparse/dim. palette good.. hints of magenta in there.
-                bdir + "space_clouds.jpg", // 16
-                adir + "green_gilled_lace.png", // 17 too green, too flickery, twinkles
-                bdir + "trippy.jpg", // 18 tame the primary red blotch!
-                adir + "red_triclops_minimal.png", // 19 primary red doesn't cut it
-                bdir + "warm_cool_weave.jpg", // 20
-                adir + "purple_chains.png", // 21 ** deep violet, maybe black holes too big in places esp Sw, subtle but tasteful twinkle effect.
-                bdir + "warped_squares.jpg", // 22
-                adir + "light_amber_trigrams.png", // ** 23 looks good, monochomatic shadowplay on rig, brings out metals like previous similar one. some black gaps too big sometimes
-                bdir + "water_orange.jpg", // 24
-                adir + "mauve_taupe_worms.png", // ** 25 not bad on metal, makes some look red and some purple, rest pink. all pink would be bad.
-                bdir + "white_yellow_flowers.jpg", // 26
-                adir + "yellow_antennae.png", // 27 looks okay on metal, not very bright, this yellow BTW reads just slightly warm esp on metal
-                bdir + "yellow_on_green_flowers.jpg", // 28
-        };
-        */
 
 
 class EffectSlot {
